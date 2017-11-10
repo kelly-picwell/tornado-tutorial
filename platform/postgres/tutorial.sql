d4f7eb5a13e685d63085212a19febbaf74a35fcc
Kelly Mathews <kelly@picwell.com>
2017-11-10 15:15:11 -0500

    remove household api

--- platform/postgres/tutorial.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS household(
-  id uuid PRIMARY KEY,
-  external_id varchar(45) NOT NULL,
-  income integer NOT NULL DEFAULT '1',
-  zip_code_3 varchar(3) NOT NULL DEFAULT '1',
-  client_name varchar(50) NOT NULL
-)
